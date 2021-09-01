d884c1dc8eb6665d1a576f1a901cacf032a56b88
wingsmight <wingsmight@gmail.com>
2021-09-01 23:56:07 +0700

    support Stop node

---
+++ Nodes/StopNode.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExcelToGraph
+{
+    public class StopNode : Node
+    {
+        public StopNode(Vector2 position, int referenceId) : base(position, referenceId)
+        {
+
+        }
+    }
+}
