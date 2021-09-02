5333e1d42adbe5f4f009fb496a68d91725070c7e
wingsmight <wingsmight@gmail.com>
2021-09-02 16:47:11 +0700

    support Character node

---
+++ Nodes/CharacterNode.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace ExcelToGraph
+{
+    public class CharacterNode : Node
+    {
+        private CharacterProperty property;
+        private Direction direction;
+
+
+        public CharacterNode(Vector2 position, int referenceId, CharacterProperty property, Direction direction) : base(position, referenceId)
+        {
+            this.property = property;
+            this.direction = direction;
+        }
+
+
+        public override string ToString()
+        {
+            string text = base.ToString() + '\n';
+            text += TAB.Multiply(4) + "character:" + '\n';
+
+            text += TAB.Multiply(5) + "name: " + property.name + '\n';
+            text += TAB.Multiply(5) + "position: " + (int)property.position + '\n';
+            text += TAB.Multiply(5) + "emotion: " + (int)property.emotion + '\n';
+
+            text += TAB.Multiply(4) + "direction: " + (int)direction + '\n';
+
+            return text;
+        }
+    }
+}
